c9a5c7f4499ba48648b13d979160e1c8954cdced
Pallab <mustakimpallab2434@gmail.com>
2022-06-01 03:16:40 +0600

    oaky okay

--- get.js
+++ get.js
@@ -3,7 +3,11 @@ module.exports = (request, response) => {
     // remove queries from the url, turn "/posts?id=0" into "/posts"
     const url = request.url.split("/")[1]
 
-    switch(url){
+    const id= request.url.split("/")[2]
+    
+    if(id){
+
+      switch(url){
 
         case "getusers":
         
@@ -11,6 +15,58 @@ module.exports = (request, response) => {
       const uri = "mongodb+srv://tryUser:1EXcFzvnOoIdlOF8@cluster0.hdlnn.mongodb.net/?retryWrites=true&w=majority";
       const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
       async function run() {
+
+        try {
+      
+          await client.connect();
+      
+          const database = client.db("insertDB");
+      
+          const userCollection = database.collection("userCollection");
+      
+          const ObjectId = require('mongodb').ObjectID;
+
+          // get document
+            
+            const query = { _id: ObjectId(id) };
+
+            const users = await userCollection.findOne(query);
+            
+            console.log(users);
+            response.statusCode = 200
+            response.setHeader("Content-Type", "application/json")
+            response.write(JSON.stringify(users))
+            response.end()
+
+        } finally {
+      
+          // await client.close();
+      
+        }
+      
+      }
+      
+      run().catch(console.dir);
+      
+      break
+        // response for unexpected get requests
+        default:
+            response.statusCode = 400
+            response.write(`CANNOT GET ${request.url}`)
+            response.end()
+            break
+
+    }
+  }
+  else{
+    switch(url){
+
+        case "getusers":
+        
+            const { MongoClient, ServerApiVersion } = require('mongodb');
+        const uri = "mongodb+srv://tryUser:1EXcFzvnOoIdlOF8@cluster0.hdlnn.mongodb.net/?retryWrites=true&w=majority";
+        const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+        async function run() {
 
         try {
       
@@ -47,4 +103,6 @@ module.exports = (request, response) => {
             break
 
     }
+  }
+    
 }
